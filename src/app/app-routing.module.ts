307b5e7bb3f2a7d54a9317a18c513ae9619dbba5
Quentin <quentin095.vaillant@gmail.com>
2022-01-05 21:55:26 +0100

    indent all project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,18 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {AuthenticationComponent} from "./components/authentication/authentication.component";
 import {AlbumComponent} from "./components/album/album.component";
 import {HomeComponent} from "./components/home/home.component";
 
 const routes: Routes = [
-    { path: 'auth', component: AuthenticationComponent },
-    { path: 'album', component: AlbumComponent },
-    { path: '', component: HomeComponent },
+    {path: 'auth', component: AuthenticationComponent},
+    {path: 'album', component: AlbumComponent},
+    {path: '', component: HomeComponent},
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/models/history.model.ts
+++ src/app/models/history.model.ts
@@ -1,8 +1,8 @@
 import {Search} from "./search.model";
 
 export interface History {
-  data: Search[];
-  count: number;
-  total: number;
-  next: string;
+    data: Search[];
+    count: number;
+    total: number;
+    next: string;
 }
